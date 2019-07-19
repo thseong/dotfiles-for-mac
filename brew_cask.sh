dca569bff826dfe82d6a1ed351cbc791fd268ef3
thseong <thseong1001@gmail.com>
2019-07-19 18:36:11 +0900

    Create .path, brew_cask.sh shell scripts

---
+++ brew_cask.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# Upgrade any already-installed cask.
+brew cask upgrade
+
+# brew cask install iterm2
+
+brew cask install google-chrome
+
+# brew cask install visual-studio
+brew cask install visual-studio-code
+# brew cask install xamarin-studio
+# brew cask install android-studio
+# brew cask install eclipse-jee
+# brew cask install intellij-idea
+# brew cask install intellij-idea-ce
+
+# brew cask install unity
+
+# brew cask install blender
+# brew cask install gimp
+# brew cask install sketch
+# brew cask install adobe-creative-cloud
+
+brew cask install marp
+# brew cask install typora
+
+brew cask install kdiff3
+# brew cask install p4v
+
+# brew cask install slack
+# brew cask install zeplin
+
+brew cask install sourcetree
+# brew cask install forklift
+brew cask install transmission
+brew cask install docker
+# brew cask install google-backup-and-sync
+# brew cask install google-drive-file-stream
+
+# brew cask install mysqlworkbench
+# brew cask install db-browser-for-sqlite
+# brew cask install mediainfo
+
+brew cask install vlc
+
+# brew cask install synergy
+brew cask install magnet
+# brew cask install caffeine
+brew cask install karabiner-elements
+# brew cask install grammarly
+brew cask install appcleaner
+
+brew tap homebrew/cask-fonts
+brew cask install font-hack-nerd-font
+brew untap homebrew/cask-fonts
