f8ca52d4d4306b924963b281230609c74f3614a9
thseong <thseong1001@gmail.com>
2019-07-19 18:24:55 +0900

    Fix brew syntax errors

--- brew.sh
+++ brew.sh
@@ -20,7 +20,7 @@ brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed -- --with-default-names
 # Install Bash 4.
 brew install bash
 brew install bash-completion2
@@ -32,13 +32,13 @@ if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
 fi;
 
 # Install `wget` with IRI support.
-brew install wget --with-iri
+brew install wget -- --with-iri
 
 # Install GnuPG to enable PGP-signing commits.
 brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim -- --with-override-system-vi
 brew install grep
 brew install openssh
 brew install screen
@@ -82,7 +82,7 @@ brew install ack
 #brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
+brew install imagemagick -- --with-webp
 brew install lua
 brew install lynx
 brew install p7zip
